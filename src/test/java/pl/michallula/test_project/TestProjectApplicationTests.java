15d38e2842aece921daf21ba889cc0b40865fe09
Michał Lula <michal.lula@lingaro.com>
2015-12-29 11:26:00 +0100

    project structure

---
+++ src/test/java/pl/michallula/test_project/TestProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michallula.test_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestProjectApplication.class)
+public class TestProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
